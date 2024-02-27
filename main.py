b9d87fefdaa6f9629f4da2d5dcbab74b59daa1cd
cynthiacruz <ccruzgz@gmail.com>
2024-02-27 10:39:40 -0600

    Write logic for encoding message.

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ while translating:
         message = input("What's the message? ")
         action = input("Do you want to encode or decode the message? ").lower()
 
+        list_of_characters = list(message)
+
         if action == "encode":
-            # TODO: write logic
+            # "Translate" the given input to morse code using the values from the morse_code dictionary:
+            message_to_morse_code = [morse_code[char] for char in message]
+
+            # Join the list into a string separating each character with a space to make decoding easier:
+            message_to_morse_code = " ".join(message_to_morse_code)
+            print(f"Here's your message. Keep it safe: {message_to_morse_code}")
         elif action == "decode":
-            # TODO: write logic
+            pass
         else:
             print("Invalid input. Try again.")
 
