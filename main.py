676c0b470892c648b1949d43c76425bc19d58ca4
cynthiacruz <ccruzgz@gmail.com>
2024-02-27 10:55:52 -0600

    Write logic for decoding. Switch message and action inputs again to avoid bug where program continued to run even with an invalid input for the action.

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ while translating:
     should_continue = input("Do you want to translate a message? Type 'Y' or 'N': ").upper()
 
     if should_continue == "Y":
-        action = input("Do you want to encode or decode the message? ").lower()
         message = input("What's the message? ")
+        action = input("Do you want to encode or decode the message? ").lower()
 
         if action == "encode":
-            # Separate the message into a list so we can search each character in the dictionary:
+            # Split characters into a list so that we can search one by one in the dictionary:
             list_of_characters = list(message)
 
             # "Translate" the given input to morse code using the values from the morse_code dictionary:
@@ -20,9 +20,19 @@ while translating:
             message_to_morse_code = " ".join(message_to_morse_code)
             print(f"Here's your message. Keep it safe: {message_to_morse_code}")
         elif action == "decode":
-            # Test message (hello) in morse code = ".... . .-.. .-.. ---"
+            # Split characters into a list:
             list_of_characters = message.split(" ")
-            print(list_of_characters)
+
+            # Search the dictionary by values so that we can get the letters (keys) for our translated message:
+            message_to_eng = []
+            for char in list_of_characters:
+                for letter, code in morse_code.items():
+                    if code == char:
+                        message_to_eng.append(letter)
+
+            # Join the translated message into a string:
+            message_to_eng = "".join(message_to_eng)
+            print(f"Here's your translated message: {message_to_eng}")
         else:
             print("Invalid input. Try again.")
 
